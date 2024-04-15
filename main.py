fd8fcd733f4b9a2240be4d4023221482e642b739
amilitaru <amilitaru@bbctech.it>
2024-04-15 14:23:20 +0300

    Added try except to the code because some error

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import cv2
 import depthai as dai
 import numpy as np
 from MultiMsgSync import TwoStageHostSeqSync
-from depthai_sdk.visualize.bbox import BoundingBox 
+from depthai_sdk.visualize.bbox import BoundingBox
+# import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+from time import sleep  # Import the sleep function from the time module
+
+# GPIO.setwarnings(False)  # Ignore warning for now
+# GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+# GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)  # Set pin 18 to be an output pin and set initial value to low (off)
+# GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)  # Set pin 18 to be an output pin and set initial value to low (off)
 
 VIDEO_SIZE = (1072, 1072)
 
@@ -12,10 +19,12 @@ pipeline = dai.Pipeline()
 pipeline.setOpenVINOVersion(version=dai.OpenVINO.Version.VERSION_2021_4)
 openvino_version = '2021.4'
 
+
 def create_output(name: str, output: dai.Node.Output):
     xout = pipeline.create(dai.node.XLinkOut)
     xout.setStreamName(name)
     output.link(xout.input)
+
 
 cam = pipeline.create(dai.node.ColorCamera)
 # For ImageManip rotate you need input frame of multiple of 16
@@ -32,10 +41,10 @@ create_output('color', cam.video)
 # ImageManip that will crop the frame before sending it to the Face detection NN node
 face_det_manip = pipeline.create(dai.node.ImageManip)
 face_det_manip.initialConfig.setResize(300, 300)
-face_det_manip.setMaxOutputFrameSize(300*300*3)
+face_det_manip.setMaxOutputFrameSize(300 * 300 * 3)
 cam.preview.link(face_det_manip.inputImage)
 
-#=================[ FACE DETECTION ]=================
+# =================[ FACE DETECTION ]=================
 
 print("Creating Face Detection Neural Network...")
 face_det_nn = pipeline.create(dai.node.MobileNetDetectionNetwork)
@@ -50,7 +59,7 @@ face_det_manip.out.link(face_det_nn.input)
 
 create_output('detection', face_det_nn.out)
 
-#=================[ SCRIPT NODE ]=================
+# =================[ SCRIPT NODE ]=================
 
 # Script node will take the output from the face detection NN as an input and set ImageManipConfig
 # to the 'age_gender_manip' to crop the initial frame
@@ -65,7 +74,7 @@ cam.preview.link(script.inputs['preview'])
 with open("script.py", "r") as f:
     script.setScript(f.read())
 
-#=================[ HEAD POSE ESTIMATION ]=================
+# =================[ HEAD POSE ESTIMATION ]=================
 
 headpose_manip = pipeline.create(dai.node.ImageManip)
 headpose_manip.initialConfig.setResize(60, 60)
@@ -83,7 +92,7 @@ headpose_manip.out.link(headpose_nn.input)
 headpose_nn.out.link(script.inputs['headpose_in'])
 headpose_nn.passthrough.link(script.inputs['headpose_pass'])
 
-#=================[ LANDMARKS DETECTION ]=================
+# =================[ LANDMARKS DETECTION ]=================
 
 landmark_manip = pipeline.create(dai.node.ImageManip)
 landmark_manip.initialConfig.setResize(48, 48)
@@ -103,8 +112,7 @@ landmark_nn.passthrough.link(script.inputs['landmark_pass'])
 
 create_output('landmarks', landmark_nn.out)
 
-
-#=================[ LEFT EYE CROP ]=================
+# =================[ LEFT EYE CROP ]=================
 
 left_manip = pipeline.create(dai.node.ImageManip)
 left_manip.initialConfig.setResize(60, 60)
@@ -113,7 +121,7 @@ script.outputs['left_manip_img'].link(left_manip.inputImage)
 script.outputs['left_manip_cfg'].link(left_manip.inputConfig)
 left_manip.out.link(script.inputs['left_eye_in'])
 
-#=================[ LEFT EYE CROP ]=================
+# =================[ LEFT EYE CROP ]=================
 
 right_manip = pipeline.create(dai.node.ImageManip)
 right_manip.initialConfig.setResize(60, 60)
@@ -122,7 +130,7 @@ script.outputs['right_manip_img'].link(right_manip.inputImage)
 script.outputs['right_manip_cfg'].link(right_manip.inputConfig)
 right_manip.out.link(script.inputs['right_eye_in'])
 
-#=================[ GAZE ESTIMATION ]=================
+# =================[ GAZE ESTIMATION ]=================
 
 gaze_nn = pipeline.create(dai.node.NeuralNetwork)
 gaze_nn.setBlobPath(blobconverter.from_zoo(
@@ -149,10 +157,9 @@ script.inputs['none'].setQueueSize(1)
 
 create_output('gaze', gaze_nn.out)
 
-#==================================================
+# ==================================================
 
 with dai.Device(pipeline, maxUsbSpeed=dai.UsbSpeed.HIGH) as device:
-    
     sync = TwoStageHostSeqSync()
 
     queues = {}
@@ -166,58 +173,74 @@ with dai.Device(pipeline, maxUsbSpeed=dai.UsbSpeed.HIGH) as device:
             if q.has():
                 msg = q.get()
                 sync.add_msg(msg, name)
-                #if name == "color":
-                    #cv2.imshow("video", msg.getCvFrame())
+                # if name == "color":
+                # cv2.imshow("video", msg.getCvFrame())
 
         msgs = sync.get_msgs()
         if msgs is not None:
             frame = msgs["color"].getCvFrame()
             dets = msgs["detection"].detections
             for i, detection in enumerate(dets):
-                det = BoundingBox(detection)
-                tl, br = det.denormalize(frame.shape)
-                cv2.rectangle(frame, tl, br, (10, 245, 10), 1)
+                try:
+                    det = BoundingBox(detection)
+                    tl, br = det.denormalize(frame.shape)
+                    cv2.rectangle(frame, tl, br, (10, 245, 10), 1)
 
-                gaze = np.array(msgs["gaze"][i].getFirstLayerFp16())
-                gaze_x, gaze_y = (gaze * 100).astype(int)[:2]
+                    gaze = np.array(msgs["gaze"][i].getFirstLayerFp16())
+                    gaze_x, gaze_y = (gaze * 100).astype(int)[:2]
 
-                landmarks = np.array(msgs["landmarks"][i].getFirstLayerFp16())
-                colors = [(0, 127, 255), (0, 127, 255), (255, 0, 127), (127, 255, 0), (127, 255, 0)]
-                for lm_i in range(0, len(landmarks) // 2):
-                    # 0,1 - left eye, 2,3 - right eye, 4,5 - nose tip, 6,7 - left mouth, 8,9 - right mouth
-                    x, y = landmarks[lm_i*2:lm_i*2+2]
-                    point = det.map_point(x,y).denormalize(frame.shape)
-                    arrowX = point[0] + gaze_x*5
-                    arrowY = point[1] - gaze_y*5
-                    # determine direction
-                    direction = ""
-                    TRESHOLD = 10
-                    if gaze_x > x:
-                        if abs(gaze_y - y) <= TRESHOLD:
-                            direction = 'left'
-                        elif gaze_y > y : 
-                            direction = 'left-up'
-                        elif gaze_y < y:
-                            direction = 'left-down'
-                    elif gaze_x < x:
-                        if abs(gaze_y - y) <= TRESHOLD:
-                            direction = 'right'
-                        elif gaze_y > y : 
-                            direction = 'right-up'
-                        elif gaze_y < y:
-                            direction = 'right-down'
-                    elif gaze_x == x:
-                        if gaze_y == y :
-                            direction = 'forward'
-                        elif gaze_y > y : 
-                            direction = 'up'
-                        elif gaze_y < y:
-                            direction = 'down'
-                    
-                    if lm_i <= 1: # Draw arrows from left eye & right eye
-                        cv2.arrowedLine(frame, point, (arrowX, arrowY), colors[lm_i], 3)
-                        cv2.putText(frame, f'Person is looking {direction}', (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-                    cv2.circle(frame, point, 2, colors[lm_i], 2)
+                    landmarks = np.array(msgs["landmarks"][i].getFirstLayerFp16())
+                    colors = [(0, 127, 255), (0, 127, 255), (255, 0, 127), (127, 255, 0), (127, 255, 0)]
+                    for lm_i in range(0, len(landmarks) // 2):
+                        # 0,1 - left eye, 2,3 - right eye, 4,5 - nose tip, 6,7 - left mouth, 8,9 - right mouth
+                        x, y = landmarks[lm_i * 2:lm_i * 2 + 2]
+                        point = det.map_point(x, y).denormalize(frame.shape)
+                        arrowX = point[0] + gaze_x * 5
+                        arrowY = point[1] - gaze_y * 5
+                        # determine direction
+                        direction = ""
+                        TRESHOLD = 10
+                        if gaze_x > x:
+                            if abs(gaze_y - y) <= TRESHOLD:
+                                direction = 'left'
+                                # GPIO.output(11, GPIO.HIGH)  # Turn on
+                                # GPIO.output(13, GPIO.LOW)  # Turn off
+                            elif gaze_y > y:
+                                direction = 'left-up'
+                                # GPIO.output(11, GPIO.LOW)  # Turn off
+                                # GPIO.output(13, GPIO.LOW)  # Turn off
+                            elif gaze_y < y:
+                                direction = 'left-down'
+                                # GPIO.output(11, GPIO.HIGH)  # Turn on
+                                # GPIO.output(13, GPIO.LOW)  # Turn off
+                        elif gaze_x < x:
+                            if abs(gaze_y - y) <= TRESHOLD:
+                                direction = 'right'
+                                # GPIO.output(13, GPIO.HIGH)  # Turn on
+                                # GPIO.output(11, GPIO.LOW)  # Turn off
+                            elif gaze_y > y:
+                                direction = 'right-up'
+                                # GPIO.output(13, GPIO.LOW)  # Turn off
+                                # GPIO.output(11, GPIO.LOW)  # Turn off
+                            elif gaze_y < y:
+                                direction = 'right-down'
+                                # GPIO.output(13, GPIO.HIGH)  # Turn on
+                                # GPIO.output(11, GPIO.LOW)  # Turn off
+                        elif gaze_x == x:
+                            if gaze_y == y:
+                                direction = 'forward'
+                            elif gaze_y > y:
+                                direction = 'up'
+                            elif gaze_y < y:
+                                direction = 'down'
+
+                        if lm_i <= 1:  # Draw arrows from left eye & right eye
+                            cv2.arrowedLine(frame, point, (arrowX, arrowY), colors[lm_i], 3)
+                            cv2.putText(frame, f'Person is looking {direction}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                        (0, 0, 0), 2)
+                        cv2.circle(frame, point, 2, colors[lm_i], 2)
+                except:
+                    continue
 
             cv2.imshow("Lasers", frame)
 
