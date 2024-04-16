b131e7fbf6fda5588269519219159d9cd2c276f7
amilitaru <amilitaru@bbctech.it>
2024-04-16 12:21:15 +0300

    Added try except to the code because some error

--- main.py
+++ main.py
@@ -44,6 +44,12 @@ face_det_manip.initialConfig.setResize(300, 300)
 face_det_manip.setMaxOutputFrameSize(300 * 300 * 3)
 cam.preview.link(face_det_manip.inputImage)
 
+flipped_face_det_manip = pipeline.create(dai.node.ImageManip)
+flipped_face_det_manip.initialConfig.setResize(300, 300)
+flipped_face_det_manip.setMaxOutputFrameSize(300 * 300 * 3)
+face_det_manip.out.link(flipped_face_det_manip.inputImage)
+
+flipped_face_det_manip.initialConfig.setMirrorFrame(True)
 # =================[ FACE DETECTION ]=================
 
 print("Creating Face Detection Neural Network...")
@@ -55,7 +61,7 @@ face_det_nn.setBlobPath(blobconverter.from_zoo(
     version=openvino_version
 ))
 # Link Face ImageManip -> Face detection NN node
-face_det_manip.out.link(face_det_nn.input)
+flipped_face_det_manip.out.link(face_det_nn.input)
 
 create_output('detection', face_det_nn.out)
 
@@ -238,6 +244,7 @@ with dai.Device(pipeline, maxUsbSpeed=dai.UsbSpeed.HIGH) as device:
                             cv2.circle(frame, point, 2, colors[lm_i], 2)
                     except:
                         continue
+                cv2.flip(frame,1)
                 cv2.imshow("Lasers", frame)
                 if cv2.waitKey(1) == ord('q'):
                     GPIO.cleanup()
